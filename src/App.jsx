166ef5595fd7c788a175288cb86e6024e058c225
rob587 <robertocamm02@yahoo.com>
2025-08-30 13:08:29 +0200

    added css

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,16 @@ function App() {
   return (
     <div className="container mt-5">
       <div className="row">
-        <div className="col-12 "><h1 className="text-danger">Lista di Articoli</h1></div>
+        <div className="col-12 ">
+          <h1 className="text-danger">Lista di Articoli</h1>
+          </div>
         <div className="col-12">
-          <ul className="list-group">
+          <ul className="list-group ">
+            <span class="border border-primary-subtle">
             {articles.map((item, index)=>{
               return (
-                <li className="list-group-item" key={index}>
-                  <div className="d-flex justify-content-between">
+                <li className="list-group-item border border-primary-subtle" key={index}>
+                  <div className="d-flex justify-content-between fw-semibold">
                     <div>{item}</div>
 
                   </div>
@@ -38,16 +41,17 @@ function App() {
                 </li>
               )
             })}
+            </span>
           </ul>
 
           <form className="mt-5 d-flex justify-content-end" onSubmit={handleForm}>
-            <input className="p-3 " type="text"
+            <input className="p-3 fs-4" type="text"
             value={newArticle}
             onChange={(event)=>{
               setNewArticle(event.target.value)
             }}
             />
-            <button className="ms-3">Inserisci nuovo articolo</button>
+            <button className="ms-3 btn btn-success fs-4">Inserisci nuovo articolo</button>
           </form>
 
         </div>
