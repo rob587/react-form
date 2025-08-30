356e8025a52f6e377828e2ca23da6c68a8f878fd
rob587 <robertocamm02@yahoo.com>
2025-08-30 11:59:07 +0200

    map and list displayed

--- src/App.jsx
+++ src/App.jsx
@@ -9,9 +9,37 @@ const articleList = [
 
 
 function App() {
+  const [article, setArticle] = useState(articleList);
+  const [newArticle, setNewArticle] = useState ("")
+
+
   return (
-    
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12 "><h1 className="text-danger">Lista di Articoli</h1></div>
+        <div className="col-12">
+          <ul className="list-group">
+            {article.map((item, index)=>{
+              return (
+                <li className="list-group-item" key={index}>
+                  <div className="d-flex justify-content-between">
+                    <div>{item}</div>
+
+
+                  </div>
+
+
+
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
   )
+    
+  
   
   
 }
